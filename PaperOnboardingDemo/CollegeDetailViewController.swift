334c7da54acdb91bae1c47cc7a8a812bb698cc49
Kunwar Sahni <kunwar.sahni01@gmail.com>
2018-01-21 10:34:25 -0500

    Added detail firebase retrival

--- PaperOnboardingDemo/CollegeDetailViewController.swift
+++ PaperOnboardingDemo/CollegeDetailViewController.swift
@@ -7,6 +7,11 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+import Firebase
+import FirebaseFirestore
+
 
 class CollegeDetailViewController: UIViewController {
 
@@ -18,12 +23,27 @@ class CollegeDetailViewController: UIViewController {
     
     @IBOutlet weak var averaeAct: UILabel!
     
-    
+    var docRef: DocumentReference!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        titleLabel.text = myArray[myIndex] as! String
+        titleLabel.text = myArray[myIndex] as? String
         
+        let db = Firestore.firestore()
+        
+        let docRef = db
+            .collection("Colleges").document("\(myArray[myIndex] as! String)")
+            .collection("CollegeData").document("Average GPA")
+        docRef.getDocument { (document, error) in
+            if let document = document {
+                print("Document data: \(document.data())")
+                //document.data = self.averageGpa.text
+            } else {
+                print("Document does not exist")
+            }
+        }
+
     }
 
 
