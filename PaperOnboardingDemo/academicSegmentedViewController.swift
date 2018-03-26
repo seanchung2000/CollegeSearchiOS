b18b3009addd0a6495f5241e58caa3000c846f21
Kunwar Sahni <kunwar.sahni01@gmail.com>
2018-03-25 21:17:06 -0400

    Onboarding

--- PaperOnboardingDemo/academicSegmentedViewController.swift
+++ PaperOnboardingDemo/academicSegmentedViewController.swift
@@ -7,11 +7,101 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+import Firebase
+import FirebaseFirestore
+import FirebaseStorage
+import GoogleMobileAds
+import SVProgressHUD
 
 class academicSegmentedViewController: UIViewController {
 
+    @IBOutlet weak var collegeDescription: UITextView!
+    @IBOutlet weak var averageGpa: UILabel!
+    @IBOutlet weak var averageSat: UILabel!
+    @IBOutlet weak var averageAct: UILabel!
+    @IBOutlet weak var acceptanceRateGraph: KDCircularProgress!
+    @IBOutlet weak var acceptanceRateLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        getCollegeData()
+    }
+    func getCollegeData() {
+        let db = Firestore.firestore()
+        let gpaRef = db
+            .collection("Colleges").document("\(myArray[myIndex] as! String)")
+            .collection("GPA")
+            .getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    for document in querySnapshot!.documents {
+                        self.averageGpa.text = "\(document.documentID)"
+                    }
+                }
+        }
+        
+        
+        let satRef = db
+            .collection("Colleges").document("\(myArray[myIndex] as! String)")
+            .collection("SAT")
+            .getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    for document in querySnapshot!.documents {
+                        self.averageSat.text = "\(document.documentID)"
+                    }
+                }
+        }
+        
+        
+        
+        let actRef = db
+            .collection("Colleges").document("\(myArray[myIndex] as! String)")
+            .collection("ACT")
+            .getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    for document in querySnapshot!.documents {
+                        self.averageAct.text = "\(document.documentID)"
+                    }
+                }
+        }
+        
+        let descriptionRef = db
+            .collection("Colleges").document("\(myArray[myIndex] as! String)")
+            .collection("Description")
+            .getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    for document in querySnapshot!.documents {
+                        self.collegeDescription.text = "\(document.documentID)"
+                    }
+                }
+        }
+        let rateRef = db
+            .collection("Colleges").document("\(myArray[myIndex] as! String)")
+            .collection("Rate")
+            .getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    for document in querySnapshot!.documents {
+                        self.acceptanceRateLabel.text = "\(document.documentID)%"
+                        //  var ratedouble: Double = document.documentID as! Double
+                        //  print(ratedouble)
+                        if let rate = Double(document.documentID) {
+                            print(rate)
+                            var degreeRate: Double = (360 * rate)/100
+                            self.acceptanceRateGraph.animate(fromAngle: 0, toAngle: degreeRate, duration: 2.5, completion: nil)
+                        }
+                    }
+                }
+        }
     }
 }
--- PaperOnboardingDemo/financialSegmentedViewController.swift
+++ PaperOnboardingDemo/financialSegmentedViewController.swift
@@ -7,29 +7,45 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+import Firebase
+import FirebaseFirestore
+import FirebaseStorage
 
 class financialSegmentedViewController: UIViewController {
-
+    @IBOutlet weak var financialLabel: UILabel!
+    @IBOutlet weak var tuitionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let db = Firestore.firestore()
+        let financialRef = db
+            .collection("Colleges").document("\(myArray[myIndex] as! String)")
+            .collection("Financial")
+            .getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                    
+                } else {
+                    for document in querySnapshot!.documents {
+                        self.financialLabel.text = "Average Financial Aid: $\(document.documentID)"
+                    }
+                }
+        }
+        
+        let tuitionRef = db
+            .collection("Colleges").document("\(myArray[myIndex] as! String)")
+            .collection("Tuition")
+            .getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    for document in querySnapshot!.documents {
+                        self.tuitionLabel.text = "Average Tuition: $\(document.documentID)"
+                    }
+                }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
